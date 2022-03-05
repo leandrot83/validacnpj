a05ebffb7db655a81840b63271d63995353f496e
leandrot83 <82677966+leandrot83@users.noreply.github.com>
2022-03-05 10:22:12 -0300

    Update validcnpj.js

--- validcnpj.js
+++ validcnpj.js
@@ -1,30 +1,28 @@
 export function cnpjValidation(value) {
   if (!value) return false
 
-  // Aceita receber o valor como string, número ou array com todos os dígitos
+  // Recebe valor como string, número ou array com todos os dígitos
   const isString = typeof value === 'string'
   const validTypes = isString || Number.isInteger(value) || Array.isArray(value)
 
-  // Elimina valor em formato inválido
+  // Elimina formato inválido
   if (!validTypes) return false
 
-  // Filtro inicial para entradas do tipo string
+  // Verifica entradas do tipo string
   if (isString) {
     // Limita ao máximo de 18 caracteres, para CNPJ formatado
     if (value.length > 18) return false
 
-    // Teste Regex para veificar se é uma string apenas dígitos válida
+    // Verifica dígitos válidos
     const digitsOnly = /^\d{14}$/.test(value)
-    // Teste Regex para verificar se é uma string formatada válida
+    // Verifica formatacao
     const validFormat = /^\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}$/.test(value)
 
-    // Se o formato é válido, usa um truque para seguir o fluxo da validação
     if (digitsOnly || validFormat) true
-    // Se não, retorna inválido
     else return false
   }
 
-  // Guarda um array com todos os dígitos do valor
+  // array com todos os dígitos do valor
   const match = value.toString().match(/\d/g)
   const numbers = Array.isArray(match) ? match.map(Number) : []
 
@@ -46,9 +44,7 @@ export function cnpjValidation(value) {
       sum += n * factor--
       if (factor < 2) factor = 9
     }
-
     const result = 11 - (sum % 11)
-
     return result > 9 ? 0 : result
   }
 
